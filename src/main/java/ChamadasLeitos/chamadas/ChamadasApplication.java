a453676154e0bf15f4c83fbae644f5b47088477d
marciojosebaldo <marciojosebaldo@gmail.com>
2023-10-17 22:31:35 -0300

    Add files via upload

--- src/main/java/ChamadasLeitos/chamadas/ChamadasApplication.java
+++ src/main/java/ChamadasLeitos/chamadas/ChamadasApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public classChamadasApplication {
+public class ChamadasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChamadasApplication.class, args);
 	}
--- src/test/java/ChamadasLeitos/chamadas/TesteChamados.java
+++ src/test/java/ChamadasLeitos/chamadas/TesteChamados.java
@@ -4,9 +4,11 @@ import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 
@@ -15,8 +17,8 @@ public class TesteChamados {
 
     @Dado("que usuário está na tela dos chamados {string}")
     public void queUsuárioEstáNaTelaDosChamados(String site) {
-        System.setProperty("webdriver.edge.driver", "driver/msedgedriver.exe");
-        driver = new EdgeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aluno\\Desktop\\BddChamaMedBruno\\driver\\chromedriver.exe"); // Aponta para o executável do ChromeDriver
+        driver = new ChromeDriver(); // Usa o ChromeDriver
         driver.get(site);
         driver.manage().window().maximize();
         throw new io.cucumber.java.PendingException();
