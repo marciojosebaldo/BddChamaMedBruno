a42137a6ad57c22a9ffc84ad623b01f7e738697b
marciojosebaldo <marciojosebaldo@gmail.com>
2023-10-30 21:18:53 -0300

    cadastrarUsuario

--- src/test/java/ChamadasLeitos/steps/cadastrarUsuario.java
+++ src/test/java/ChamadasLeitos/steps/cadastrarUsuario.java
@@ -4,19 +4,34 @@ import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
 
 public class cadastrarUsuario {
+
+    public WebDriver driver;
+
     @Dado("que o usuario esta na tela {string}")
     public void queOUsuarioEstaNaTela(String site) {
-
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\Aluno\\Desktop\\msedgedriver"); // Aponta para o executável do ChromeDriver
+        driver = new EdgeDriver();
+        driver.get(site);
     }
 
     @Quando("o usuario clica no botao Adicionar Usuario e abre uma nova janela {string}")
-    public void oUsuarioClicaNoBotaoAdicionarUsuarioEAbreUmaNovaJanela(String arg0) {
+    public void oUsuarioClicaNoBotaoAdicionarUsuarioEAbreUmaNovaJanela(String novaJanela) {
+        WebElement clicaBotao = driver.findElement(By.className("btn btn-primary"));
+        clicaBotao.click();
+        String janelaCadastro = driver.getCurrentUrl();
+        Assert.assertEquals(novaJanela, janelaCadastro);
     }
 
-    @E("digitar o nome {string}, número de matricula {string} e selecionar o cargo {string}")
-    public void digitarONomeNúmeroDeMatriculaESelecionarOCargo(String arg0, String arg1, String arg2) {
+    @E("digitar o nome {string}, numero de matricula {string} e selecionar o cargo {string}")
+    public void digitarONomeNumeroDeMatriculaESelecionarOCargo(String novoNome, String novaMatricula, String novoCargo) {
+
     }
 
     @E("clica no botao Salvar Cadastro")
--- src/test/resources/features/cadastrarUsuario.feature
+++ src/test/resources/features/cadastrarUsuario.feature
@@ -3,6 +3,6 @@
     Cenario: usuario acessara a aplicacao e preenhcera os dados necessarios para cadastrar um usuario
       Dado que o usuario esta na tela "http://localhost:8080/showUsuario"
       Quando o usuario clica no botao Adicionar Usuario e abre uma nova janela "http://localhost:8080/showNewUsuarioForm"
-      E digitar o nome "novoNome", número de matricula "1234" e selecionar o cargo "Gestor"
+      E digitar o nome "novoNome", numero de matricula "1234" e selecionar o cargo "Gestor"
       E clica no botao Salvar Cadastro
       Entao sera salvo um novo usuario e retorna para a janela anterior "http://localhost:8080/showUsuario"
